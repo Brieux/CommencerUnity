be5c4e832cea42d948cd99ca7becf4e186ba5e57
Brieux "Thazerr" Caquelin <33527111+Brieux@users.noreply.github.com>
2021-10-05 18:04:28 +0200

    Initial commit

---
+++ Assets/Movement.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public int Speed;
+    public bool OnGround;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponentInParent<Rigidbody>().velocity = new Vector3(Input.GetAxis("Horizontal") * Speed, GetComponentInParent<Rigidbody>().velocity.y, 0);
+        if (Input.GetKeyDown(KeyCode.Space) && (OnGround == true))
+        {
+            GetComponentInParent<Rigidbody>().velocity = new Vector3(0, 10, 0);
+            OnGround = false;
+        }
+    }
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        {
+            OnGround = true;
+        }
+    }
+}
